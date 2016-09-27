8f2a52cd89678a0cafa2378e2ec8245b0a507756
Frumbasjt <richardgankema@gmail.com>
2016-09-27 12:19:22 +0200

    Tutorial

--- src/main/java/vu/lsde/App.java
+++ src/main/java/vu/lsde/App.java
@@ -1,13 +1,31 @@
 package vu.lsde;
 
+import org.apache.spark.api.java.*;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        String logFile = "hdfs://test.txt"; // Should be some file on your system
+        SparkConf conf = new SparkConf()
+                .setAppName("Simple Application")
+                .setMaster("local[*]");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> logData = sc.textFile(logFile).cache();
+
+        long numAs = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) { return s.contains("a"); }
+        }).count();
+
+        long numBs = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) { return s.contains("b"); }
+        }).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
     }
 }
