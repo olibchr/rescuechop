3922baccab0ec753199ff695a87176793aa9ca98
Frumbasjt <richardgankema@gmail.com>
2016-09-27 10:47:32 +0200

    Maven set up

---
+++ src/main/java/vu/lsde/App.java
@@ -0,0 +1,13 @@
+package vu.lsde;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
