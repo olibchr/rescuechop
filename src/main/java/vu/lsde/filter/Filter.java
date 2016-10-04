864c55897f7177f46e8aa8de2e877acd8374dc69
Oliver Becher <oliver.becher@gmail.com>
2016-10-04 12:41:15 +0200

    modified:   src/main/java/vu/lsde/filter/Filter.java

--- src/main/java/vu/lsde/filter/Filter.java
+++ src/main/java/vu/lsde/filter/Filter.java
@@ -3,39 +3,57 @@ package vu.lsde.filter;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
+import scala.Tuple2;
+import vu.lsde.core.Config;
 import vu.lsde.core.model.SensorDatum;
 import org.opensky.libadsb.msgs.VelocityOverGroundMsg;
+import java.io.IOException;
+
+
 
 /**
  * Created by oliverbecher on 10/3/16.
  */
+
+SString type, Double lat, Double lon, Double alt, double timeServer, Double timeSensor,
+        Double timestamp, String rawMessage, int serialNumber, Double rssiPacket, Double rssiPreamble,
+        Double snr, Double confidence
+
+
 public class Filter {
     public static void main(String[] args) throws IOException {
+
+        String inputPath = "testSamplerOut3Part3.csv";
+//        String outputPath = Config.OPEN_SKY_SAMPLE_DATA_PATH;
+        String outputPath = args[0];
+
         SparkConf sparkConf = new SparkConf().setAppName("LSDE09 Filter");
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
 
-        JavaRDD<String> csvData = sc.textFile("testSamplerOut3Part3.csv");
+        JavaRDD<String> csvData = sc.textFile(inputPath);
 
         JavaRDD<SensorDatum> rdd_records = sc.textFile(csvData).map(new Function <String, SensorDatum>(){
             public SensorDatum call(String line) throws Exception{
                 String[] fields = line.split(",");
-                SensorDatum sd = new SensorDatum(fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], fields[8], fields[9], fields[10], fields[11], fields[12]);
+                SensorDatum sd = new SensorDatum(String(null), Double(null), Double(null), Double(null), double.fields[0], double(fields[1]), double(fields[2]), String(fields[3]), int(fields[4]);
+
                 return sd;
             }});
 
 
-        JavaPairRDD<String, Iterable<SensorDatum>> sensorDataByAircraft = sensorData.groupBy(new Function<SensorDatum, String>() {
+        JavaPairRDD<String, Iterable<SensorDatum>> sensorDataByAircraft = rdd_records.groupBy(new Function<SensorDatum, String>() {
             public String call(SensorDatum sensorDatum) {
                 return sensorDatum.icao;
             }
         });
 
         // sort by timeatserver
-        JavaPairRDD<String, Iterable<SensorDatum>>  sortedICAOs = sensorDataByAircraft.sortByKey(new Function<SensorDatum, String>() {
-            public String call(SensorDatum sensorDatum) {
+        JavaPairRDD<String, Iterable<SensorDatum>>  sortedICAOs = sensorDataByAircraft.sortByKey(new Function<Iterable<SensorDatum>, String>() {
+            public double call(SensorDatum sensorDatum) {
                 return sensorDatum.timeAtServer;
             }
         });
